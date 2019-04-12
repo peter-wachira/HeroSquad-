6aced2e95062c83fb701ad3ed1d73ec84bbfe73e
pwachira900@gmail.com <pwachira900@gmail.com>
2019-04-12 17:42:47 +0300

    created main method in app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,4 +4,9 @@ import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+
+    }
 }

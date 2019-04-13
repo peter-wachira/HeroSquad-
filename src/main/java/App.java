f60a7ae152efaf784084847f6ea6e9c65a27a31a
pwachira900@gmail.com <pwachira900@gmail.com>
2019-04-13 17:26:10 +0300

    made herp  form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 import java.util.HashMap;
 import java.util.Map;
 import spark.ModelAndView;
+import java.util.ArrayList;
+import java.util.List;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 public class App {
@@ -13,28 +15,42 @@ public class App {
             model.put("template", "templates/index.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
+
         get("/form", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             model.put("template", "templates/heroForm.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
-//        post("/heroes", (request, response) -> {
+
+//        post("/heroes", (request,response) -> {
 //            Map<String, Object> model = new HashMap<String, Object>();
-//
 //            String name = request.queryParams("name");
-//            String age = request.queryParams("age");
-//            String power = request.queryParams("power");
-//            String weakness = request.queryParams("weakness");
-//
-//            Hero newHero = new Hero(name,Integer.parseInt(age),power,weakness);
-//            request.session().attribute("hero", newHero);
-//
+//            Hero newHero = new Hero(name,Integer.parseInt("age"),"power","weakness");
 //            model.put("template", "templates/heroes.vtl");
-//            model.put("hero", request.session().attributes("hero"));
 //            return new ModelAndView(model, layout);
 //        }, new VelocityTemplateEngine());
 
+        post("/heroes", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+
+            ArrayList<Hero> heroes = request.session().attribute("heroes");
+            if (heroes == null) {
+                heroes = new ArrayList<Hero>();
+                request.session().attribute("heroes", heroes);
+            }
+
+            String name = request.queryParams("name");
+            String age = request.queryParams("age");
+            String power = request.queryParams("power");
+            String weakness = request.queryParams("weakness");
+            Hero newHero = new Hero(name,Integer.parseInt(age),power,weakness);
+            heroes.add(newHero);
+
+            model.put("template", "templates/heroes.vtl");
+            model.put("heroes", request.session().attribute("heroes"));
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
 
     }
 }

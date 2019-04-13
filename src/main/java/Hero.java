eaa0388424296fad402e183d5b815333a4e08bef
pwachira900@gmail.com <pwachira900@gmail.com>
2019-04-13 15:23:55 +0300

    test name argument

---
+++ src/main/java/Hero.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Hero {
+
+    private String mName;
+    private int mAge;
+    private String mPower;
+    private String mWeakness;
+
+
+
+    public Hero(String name,int age,String power,String weakness ) {
+        mName = name;
+        mAge = age;
+        mPower = power;
+        mWeakness = weakness;
+    }
+
+    public String getName() {
+        return mName;
+    }
+    public int getAge() {
+        return mAge;
+    }
+    public String getPower() {
+        return mPower;
+    }
+
+    public String getWeakness() {
+        return mWeakness;
+    }
+}

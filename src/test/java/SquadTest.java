272a1dbfe6772be02e958be7aabae339396d3d18
pwachira900@gmail.com <pwachira900@gmail.com>
2019-04-14 12:58:18 +0300

    add test for instanciating Squad class

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -1,4 +1,10 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 public class SquadTest {
+
+    @Test
+    public void squad_instantiatesCorrectly_true() {
+        Squad testSquad = new Squad("Chauvinists",4,"Equality of sexes");
+        assertEquals(true, testSquad instanceof Squad);
+    }
 }

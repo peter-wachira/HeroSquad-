33050deaf3140e7627686bfdd18d853b4adc5bd8
pwachira900@gmail.com <pwachira900@gmail.com>
2019-04-14 14:54:47 +0300

    add get methods for fetching from squadHeroes form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -50,6 +50,50 @@ public class App {
         }, new VelocityTemplateEngine());
 
 
+        // route to serve the form for adding new squads
+        get("squads/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/squadForm.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+        // gather input from squad form
+        post("/squads", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String name = request.queryParams("name");
+            int size = Integer.parseInt(request.queryParams("size"));
+            String cause = request.queryParams("cause");
+            Squad newSquad= new Squad(name,size,cause);
+            model.put("template", "templates/squadSuccess.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+        //route to handle squads
+        get("/squads", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("squads", Squad.all());
+            model.put("template", "templates/squads.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        //route to handle displaying individual category page
+        get("/squads/:id",(request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            Squad squad = Squad.find(Integer.parseInt(request.params(":id")));
+            model.put("squad", squad);
+            model.put("template", "templates/squad.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+        // route to handle a form for adding new heroes to squadss.
+        get("squads/:id/heroes/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            Squad squad = Squad.find(Integer.parseInt(request.params(":id")));
+            model.put("squad", squad);
+            model.put("template", "templates/squadHeroesForm.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+
+
+
 
 
 

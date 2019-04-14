75596ce3591395132444c262f38742e107c0681a
pwachira900@gmail.com <pwachira900@gmail.com>
2019-04-14 12:47:35 +0300

    add class squad constructor and methods

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,20 +16,43 @@ public class App {
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
-        get("/form", (request, response) -> {
+
+
+
+
+
+
+
+
+
+
+//        get("/form", (request, response) -> {
+//            Map<String, Object> model = new HashMap<String, Object>();
+//            model.put("template", "templates/heroForm.vtl");
+//            return new ModelAndView(model, layout);
+//        }, new VelocityTemplateEngine());
+
+        // getting instances of Hero class
+        get("/heroes", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            model.put("template", "templates/heroForm.vtl");
+            model.put("heroes", Hero.all());
+            model.put("template", "templates/heroes.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        //getting heroe by their id
+        get("/heroes/:id", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            Hero hero = Hero.find(Integer.parseInt(request.params(":id")));
+            model.put("hero", hero);
+            model.put("template", "templates/hero.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
 
-//        post("/heroes", (request,response) -> {
-//            Map<String, Object> model = new HashMap<String, Object>();
-//            String name = request.queryParams("name");
-//            Hero newHero = new Hero(name,Integer.parseInt("age"),"power","weakness");
-//            model.put("template", "templates/heroes.vtl");
-//            return new ModelAndView(model, layout);
-//        }, new VelocityTemplateEngine());
+
+
+
 
         post("/heroes", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
